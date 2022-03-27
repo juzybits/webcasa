ba19c7451ad3110fa508831d8fec54a9b91af129
juzybits <96890594+juzybits@users.noreply.github.com>
2022-03-27 11:16:33 +0000

    Table overview

--- src/js/ViewOverview.tsx
+++ src/js/ViewOverview.tsx
@@ -4,20 +4,22 @@ import { getShortMasterSecret } from "./_util";
 
 export class ViewOverview extends React.Component {
     render() {
-        var balance = this.props.wallet.getBalance().toString();
-        var data = this.props.wallet.getContents();
+        const balance = this.props.wallet.getBalance().toString();
+        const data = this.props.wallet.getContents();
         return (
             <div id="ViewOverview" className="pure-u">
-                <pre>
-                    <br/> master secret: {getShortMasterSecret(this.props.wallet)}
-                    <br/> balance: {balance}
-                    <br/> version: {JSON.stringify(data.version, null, 4)}
-                    <br/> terms: {data.legalese === true ? 'yes' :'no'}
-                    <br/> depths: {JSON.stringify(data.walletdepths, null, 4)}
-                    <br/> webcash: {data.webcash.length}
-                    <br/> unconfirmed: {data.unconfirmed.length}
-                    <br/> log: {data.log.length}
-                </pre>
+            <table className="pure-table">
+            <tbody>
+                <tr><td>master secret:</td><td><pre>{getShortMasterSecret(this.props.wallet)}</pre></td></tr>
+                <tr><td>balance:</td><td>{balance}</td></tr>
+                <tr><td>depths:</td><td>{JSON.stringify(data.walletdepths,null,4)}</td></tr>
+                <tr><td>log:</td><td>{data.log.length}</td></tr>
+                <tr><td>webcashes:</td><td>{data.webcash.length}</td></tr>
+                <tr><td>unconfirmed:</td><td>{data.unconfirmed.length}</td></tr>
+                <tr><td>terms accepted:</td><td>{data.legalese===true?'yes':'no'}</td></tr>
+                <tr><td>version:</td><td>{JSON.stringify(data.version,null,4)}</td></tr>
+            </tbody>
+            </table>
             </div>
         );
     }
