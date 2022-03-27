db58d57ef9bdebe257811e907aaa19d26452f6f4
juzybits <96890594+juzybits@users.noreply.github.com>
2022-03-26 23:12:54 -0100

    Toggle menu

--- src/js/Navigation.tsx
+++ src/js/Navigation.tsx
@@ -3,18 +3,24 @@ import React from "react";
 export class Navigation extends React.Component {
     constructor(props) {
         super(props);
+        this.toggleVisibility = this.toggleVisibility.bind(this);
+        this.state = { visible: false }
+    }
+
+    toggleVisibility() {
+        this.setState({ visible: !this.state.visible });
     }
 
     render() {
         return (
-            <div id="nav" className="pure-u">
+            <div id="nav" className={"pure-u " + (this.state.visible ? "active" : '')}>
 
                 <label id="logo">🐮 CowCash</label>
 
-                <a href="#" id="menuLink">☰</a>
+                <a href="#" id="menuToggle" onClick={this.toggleVisibility}>☰</a>
 
                 <div className="nav-inner">
-                    {/*<button className="primary-button pure-button">Connect</button> -->*/}
+                    {/*<button className="primary-button pure-button">Connect</button>*/}
                     <div className="pure-menu">
                         <ul className="pure-menu-list">
                             <MenuItem name="Wallet"/>
@@ -37,53 +43,3 @@ class MenuItem extends React.Component {
         );
     }
 }
-
-/*
-function getElements() {
-    return {
-        menu: document.getElementById('nav'),
-        menuLink: document.getElementById('menuLink')
-    };
-}
-
-function toggleClass(element, className) {
-    var classes = element.className.split(/\s+/);
-    var length = classes.length;
-    var i = 0;
-
-    for (; i < length; i++) {
-        if (classes[i] === className) {
-            classes.splice(i, 1);
-            break;
-        }
-    }
-    // The className is not found
-    if (length === classes.length) {
-        classes.push(className);
-    }
-
-    element.className = classes.join(' ');
-}
-
-function toggleMenu() {
-    var active = 'active';
-    var elements = getElements();
-
-    toggleClass(elements.menu, active);
-}
-
-function handleEvent(e) {
-    var elements = getElements();
-
-    if (e.target.id === elements.menuLink.id) {
-        toggleMenu();
-        e.preventDefault();
-    } else if (elements.menu.className.indexOf('active') !== -1) {
-        toggleMenu();
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    document.addEventListener('click', handleEvent);
-});
-*/
