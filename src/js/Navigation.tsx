ac65847712fec91079ebb4c1e36636c533f38b46
juzybits <96890594+juzybits@users.noreply.github.com>
2022-03-27 20:29:51 +0000

    Add counts to Log, Webcashes, and Unconfirmed views

--- src/js/Navigation.tsx
+++ src/js/Navigation.tsx
@@ -16,7 +16,7 @@ export class Navigation extends React.Component {
 
     render() {
         const menuItems = ["Overview", "Log"].map((item) =>
-            <MenuItem key={item} name={item} onClick={this.props.handleMenuClick}/>
+            <MenuItem key={item} name={item} wallet={this.props.wallet} onClick={this.props.handleMenuClick}/>
         );
         return (
             <div id="nav" className={"pure-u " + (this.state.visible ? "active" : '')}>
@@ -48,9 +48,24 @@ function MenuItem(props) {
         event.preventDefault();
         props.onClick(props.name);
     };
+
+    let extra = ''; // add counts like: "Log (72)"
+    if ("Log" === props.name) {
+        extra = props.wallet.getContents().log.length;
+    } else
+    if ("Webcashes" === props.name) {
+        extra = props.wallet.getContents().webcash.length;
+    } else
+    if ("Unconfirmed" === props.name) {
+        extra = props.wallet.getContents().unconfirmed.length;
+    }
+    if (extra !== '') {
+        extra = <span class="email-count"> ({extra})</span>;
+    }
+
     return (
         <li className="pure-menu-item">
-            <a href="#" className="pure-menu-link" onClick={handleClick}>{props.name}</a>
+            <a href="#" className="pure-menu-link" onClick={handleClick}>{props.name}{extra}</a>
         </li>
     );
 }
