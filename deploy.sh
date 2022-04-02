a64a68e5fbca178c861baaa701fc1d30b1327e8a
juzybits <96890594+juzybits@users.noreply.github.com>
2022-04-02 22:48:23 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 DIST_DIR=../dist
 
 rm -Rf $DIST_DIR
-npx parcel build src/index.html --no-source-maps --public-url https://wayfzbee.github.io/dist-webcasa
+rm -Rf .parcel-cache/
+
+npx parcel build src/index.html --no-source-maps --public-url https://wayfzbee.github.io/webcasa-dist/
 mv dist $DIST_DIR
 cd $DIST_DIR
 
 git init .
-git remote add origin git@github.com:wayfzbee/dist-webcasa.git
+git remote add origin git@github.com:wayfzbee/webcasa-dist.git
 git add .
-git commit -am"Initial commit"
+git commit -am"init"
 git push -f -u origin master
