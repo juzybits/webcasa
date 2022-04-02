6ff400cb264eb609a0906095b57d09e78cc9b0b0
juzybits <96890594+juzybits@users.noreply.github.com>
2022-04-02 00:55:41 +0000

    Finish up ViewLog

--- src/js/ViewLog.tsx
+++ src/js/ViewLog.tsx
@@ -33,44 +33,67 @@ export class ViewLog extends React.Component {
         return <div className="copiable-group">{copiableValues}</div>
     }
     render() {
-
         let key = 0;
-        const logs = this.props.logs.slice(0).reverse().map((x) =>
-                <div className="list-item" key={key++}>
-                    <div className="list-item-row">
-                        <label className="item-label">type:</label>
-                        <CopiableValue contents={x.type} />
-                    </div>
-                    <div className="list-item-row">
-                        <label className="item-label">amount:</label>
-                        <CopiableValue contents={x.amount} />
-                    </div>
-                    <div className="list-item-row">
-                        <label className="item-label">memo:</label>
-                        <CopiableValue contents={x.memo} />
-                    </div>
-                    <div className="list-item-row">
-                        <label className="item-label">time:</label>
-                        <CopiableValue contents={x.timestamp} />
-                    </div>
-                    <div className="list-item-row">
-                        <label className="item-label">webcash:</label>
-                        {this.webcashToCopiableValues(x.webcash)}
-                    </div>
-                    <div className="list-item-row">
-                        <label className="item-label">outputs:</label>
-                        {this.webcashToCopiableValues(x.output_webcash)}
-                    </div>
-                    <div className="list-item-row">
-                        <label className="item-label">new_webcash:</label>
-                        {this.webcashToCopiableValues(x.input_webcash)}
-                    </div>
-                    <div className="list-item-row">
-                        <label className="item-label">inputs:</label>
-                        {this.webcashToCopiableValues(x.input_webcash)}
-                    </div>
-                </div>
-            );
+        const logs = this.props.logs.slice(0).reverse().map((x) => {
+
+            const row_type = !x.type ? '' :
+            <div className="list-item-row">
+                <label className="item-label">type:</label>
+                <CopiableValue contents={x.type} />
+            </div>;
+
+            const row_amount = !x.amount ? '' :
+            <div className="list-item-row">
+                <label className="item-label">amount:</label>
+                <CopiableValue contents={x.amount} />
+            </div>;
+
+            const row_memo = !x.memo ? '' :
+            <div className="list-item-row">
+                <label className="item-label">memo:</label>
+                <CopiableValue contents={x.memo} />
+            </div>;
+
+            const row_timestamp = !x.timestamp ? '' :
+            <div className="list-item-row">
+                <label className="item-label">time:</label>
+                <CopiableValue contents={x.timestamp} />
+            </div>;
+
+            const row_webcash = !x.webcash ? '' :
+            <div className="list-item-row">
+                <label className="item-label">webcash:</label>
+                {this.webcashToCopiableValues(x.webcash)}
+            </div>;
+
+            const row_input_webcash = !x.input_webcash ? '' :
+            <div className="list-item-row">
+                <label className="item-label">inputs:</label>
+                {this.webcashToCopiableValues(x.input_webcash)}
+            </div>;
+
+            const row_output_webcash = !x.output_webcash ? '' :
+            <div className="list-item-row">
+                <label className="item-label">outputs:</label>
+                {this.webcashToCopiableValues(x.output_webcash)}
+            </div>;
+
+            const row_new_webcash = !x.new_webcash ? '' :
+            <div className="list-item-row">
+                <label className="item-label">new_webcash:</label>
+                {this.webcashToCopiableValues(x.new_webcash)}
+            </div>;
+            return <div className="list-item" key={key++}>
+                {row_type}
+                {row_amount}
+                {row_memo}
+                {row_timestamp}
+                {row_webcash}
+                {row_input_webcash}
+                {row_output_webcash}
+                {row_new_webcash}
+            </div>
+        });
         const wallet = this.props.wallet;
         return (
             <div id="ViewLog" className="pure-u card">
