bfdab39ea054968d1d6e5d65fd310232cb1869e0
juzybits <96890594+juzybits@users.noreply.github.com>
2022-04-11 04:12:03 +0000

    Style webcash.org terms

--- src/js/ViewTerms.tsx
+++ src/js/ViewTerms.tsx
@@ -5,56 +5,64 @@ export class ViewTerms extends React.Component {
     constructor(props) {
         super(props)
         this.onCheckboxChange = this.onCheckboxChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
         this.state = {
-            accept: true,
+            casaTerms: false,
+            cashTerms: false,
+            webcashTermsText: 'Fetching from webcash.org (please wait)',
         };
+        // TODO: pull webcash terms text
     }
 
     onCheckboxChange(event) {
-        this.setState({accept: event.target.checked});
+        const target = event.target;
+        this.setState({
+            [target.value]: target.checked
+        });
     }
 
-    onSubmit() {
-        event.preventDefault();
-        if (this.state.accept) {
-            this.props.onAcceptTerms();
-        } else {
-            alert("Please read and accept the terms");
-        }
+    makeCasaTerms() {
+        return (
+        <React.Fragment>
+            <h1>🏠 Welcome to WebCasa<sup>(beta)</sup></h1>
+            <p>
+                WebCasa is a browser wallet for <a href="https://webcash.org" target="_blank">Webcash</a> (not affiliated).
+            </p>
+            <p>
+                This is a non-custodial wallet. You are responsible for securing and backing up your wallet file.
+            </p>
+            <p>
+                WebCasa is experimental software. It is offered for free and without any guarantees. There could be bugs so please use with care.
+            </p>
+            <div className="check-item">
+                <input type="checkbox" id="casaTerms-chk" value="casaTerms" onChange={this.onCheckboxChange}/>
+                <label htmlFor="casaTerms-chk">I understand.</label>
+            </div>
+        </React.Fragment>
+        );
+    }
+
+    makeCashTerms() {
+        return (
+        <React.Fragment>
+            <h1>Webcash terms of service</h1>
+            <p>
+                Even though WebCasa is not affiliated with Webcash, you must read and accept the Webcash.org <a href="https://webcash.org/terms" target="_blank">terms of service</a> before using the software.
+            </p>
+            <textarea class="modal-textarea" defaultValue={this.state.webcashTermsText} />
+            <div className="check-item">
+                <input type="checkbox" id="cashTerms-chk" value="cashTerms" onChange={this.onCheckboxChange}/>
+                <label htmlFor="cashTerms-chk">I agree to the Webcash.org <a href="https://webcash.org/terms" target="_blank">terms of service</a>.</label>
+            </div>
+        </React.Fragment>
+        );
     }
 
     render() {
-        const webcashTerms = "TODO pull terms from webcash.org";
+        const terms = !this.state.casaTerms ? this.makeCasaTerms() : this.makeCashTerms();
         return (
         <div id="ViewTerms" className="modal">
             <div className="modal-card">
-                <h1>🏠 Welcome to WebCasa<sup>(beta)</sup></h1>
-                <p>
-                    WebCasa is a browser wallet for <a href="https://webcash.org" target="_blank">Webcash</a> (not affiliated).
-                </p>
-                <p>
-                    This is a non-custodial wallet. You are responsible for securing and backing up your wallet file.
-                </p>
-                <p>
-                    WebCasa is experimental software. It is offered for free and without any guarantees. There could be bugs so please use with care.
-                </p>
-                <div className="check-item">
-                    <input type="checkbox" id="accept-terms-chk" value="accept" onChange={this.onCheckboxChange}/>
-                    <label htmlFor="accept-terms-chk">I understand</label>
-                </div>
-                {/*
-                <p>
-                    Even though WebCasa is not affiliated with Webcash, you must read and accept the Webcash.org <a href="https://webcash.org/terms" target="_blank">Terms of Service</a> before continuing.
-                </p>
-                <textarea class="modal-textarea">
-                    {webcashTerms}
-                </textarea>
-                <div className="check-item">
-                    <input type="checkbox" id="accept-terms-chk" value="accept" onChange={this.onCheckboxChange}/>
-                    <label htmlFor="accept-terms-chk">I acknowledge and agree to the Webcash.org <a href="https://webcash.org/terms" target="_blank">Terms of Service</a></label>
-                </div>
-                */}
+                {terms}
             </div>
         </div>
         );
