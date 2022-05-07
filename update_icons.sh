0485dd7e5736bbb2b1011dbc4028d3dce60defd4
juzybits <96890594+juzybits@users.noreply.github.com>
2022-05-07 20:25:49 +0000

    update_icons.sh + update icons

---
+++ update_icons.sh
@@ -0,0 +1,6 @@
+cd $HOME/Downloads
+unzip fontello-*.zip
+rm fontello-*.zip
+rm fontello-*/demo.html
+rm -rf $HOME/data/repos/webcash/casa/src/css/icons/
+mv fontello-*/ $HOME/data/repos/webcash/casa/src/css/icons
