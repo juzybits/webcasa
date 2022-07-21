6c7ff9b03aa57e126bf06550490e4f9f479c92a6
juzybits <96890594+juzybits@users.noreply.github.com>
2022-07-21 02:12:56 +0000

    Add icon-gift-solid

--- update_icons.sh
+++ update_icons.sh
@@ -2,5 +2,6 @@ cd $HOME/Downloads
 unzip fontello-*.zip
 rm fontello-*.zip
 rm fontello-*/demo.html
+rm fontello-*/LICENSE.txt
 rm -rf $HOME/data/repos/webcash/casa/src/css/icons/
 mv fontello-*/ $HOME/data/repos/webcash/casa/src/css/icons
